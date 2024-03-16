448c34c0fb8d7f184d313fceaf2600b90e873a67
Madina-kh16 <madinahacieva007@mail.ru>
2024-03-16 16:57:01 +0300

    changes degree

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="blocks">
       {/* <Counter /> */}
-      {/* <Degree /> */}
+      <Degree />
       {/* <Button /> */}
       {/* <Calculator /> */}
       {/* <Todo /> */}
--- src/tasks/task-2/degree.js
+++ src/tasks/task-2/degree.js
@@ -1,5 +1,33 @@
+import { useState } from "react";
+
 const Degree = () => {
-  return <div></div>;
+  const [value1, setValue1] = useState(0);
+  const [value2, setValue2] = useState(0);
+  const changeValues = (e) => {
+    setValue1(e.target.value);
+    setValue2(((e.target.value - 9) * 5) / 32);
+  };
+
+  const changeValues2 = (e) => {
+    setValue2(e.target.value);
+    setValue1(((e.target.value - 32) * 5) / 9);
+  };
+  return (
+    <div>
+      <input
+        className="input"
+        type="text"
+        value={value1}
+        onChange={changeValues}
+      />
+      <input
+        className="input"
+        type="text"
+        value={value2}
+        onChange={changeValues2}
+      />
+    </div>
+  );
 };
 
 export default Degree;
